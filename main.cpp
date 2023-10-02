1332db8101c22bf6ed8fa5316fe4b9126e1241dd
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-10-02 12:40:04 +0900

    char型の直し

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 ﻿#include<stdio.h>
 
-template <typename Type>	
+template <typename Type, typename Type2>
 
 //値を比べて小さい値を返す
-Type Min(Type a, Type b)	
+Type Min(Type a, Type2 b)
 {
 	if (a < b)
 	{
@@ -19,31 +19,21 @@ template<>	//テンプレ解除
 
 char Min<char>(char a, char b)	//char型のみ個別対応
 {
-	return printf("数字以外は代入できません\n");
+	printf("数字以外は代入できません\n");
+	return 0;
 }
 
-int intNumA = 114;
-int intNumB = 514;
-
-float floatNumA = 11.4f;
-float floatNumB = 51.4f;
-
-double doubleNumA = 3.1;
-double doubleNumB = 4.5;
-
-char charNumA = 2;
-char charNumB = 4;
 
 int main()
 {
 	//int型
-	printf("%d\n", Min<int>(intNumA, intNumB));
+	printf("%d\n", Min<int>(114, 514));
 	//float型
-	printf("%f\n", Min<float>(floatNumA, floatNumB));
+	printf("%f\n", Min<float>(11.4f, 51.4f));
 	//double型
-	printf("%lf\n", Min<double>(doubleNumA, doubleNumB));
+	printf("%lf\n", Min<double>(3.1, 4.5));
 	//char型
-	printf("%c\n", Min<char>(charNumA, charNumB));
+	Min<char>('a', 'b');
 
 
 	return 0;
