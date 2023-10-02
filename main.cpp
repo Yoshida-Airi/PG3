403f831fe5a0d6620fb146d42af8b93bc7496d99
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-10-02 11:25:16 +0900

    01_01

--- main.cpp
+++ main.cpp
@@ -1,8 +1,49 @@
-#include<stdio.h>
+﻿#include<stdio.h>
+
+template <typename Type>	
+
+//値を比べて小さい値を返す
+Type Min(Type a, Type b)	
+{
+	if (a < b)
+	{
+		return static_cast<Type>(a);
+	}
+	else if (a > b)
+	{
+		return static_cast<Type>(b);
+	}
+}
+
+template<>	//テンプレ解除
+
+char Min<char>(char a, char b)
+{
+	return printf("数字以外は代入できません\n");
+}
+
+int intNumA = 114;
+int intNumB = 514;
+
+float floatNumA = 11.4f;
+float floatNumB = 51.4f;
+
+double doubleNumA = 3.1;
+double doubleNumB = 4.5;
+
+char charNumA = 2;
+char charNumB = 4;
 
 int main()
 {
-
+	//int型
+	printf("%d\n", Min<int>(intNumA, intNumB));
+	//float型
+	printf("%f\n", Min<float>(floatNumA, floatNumB));
+	//double型
+	printf("%lf\n", Min<double>(doubleNumA, doubleNumB));
+	//char型
+	printf("%c\n", Min<char>(charNumA, charNumB));
 
 
 	return 0;
