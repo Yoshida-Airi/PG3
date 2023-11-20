2206302a29a754a3effd8300c2f697c13e41c938
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-11-20 12:06:57 +0900

    u-n

--- main.cpp
+++ main.cpp
@@ -8,27 +8,36 @@
 
 
 // サイコロの出目をランダムに生成する関数
-int rollDice(std::mt19937 randomEngine)
-{
+std::function<int()> rollDice = []() {
+
+    // 乱数生成器の初期化
+    std::random_device seedGenerator;
+    std::mt19937 randomEngine(seedGenerator());
     std::uniform_int_distribution<int> distribution(1, 6);
     return distribution(randomEngine);
+};
+
+
+int SetTimeout(std::function<int()> func, int time)
+{
+    std::this_thread::sleep_for(std::chrono::seconds(time));    //時間分待つ
+    return func();
 }
 
 // ゲームのメイン関数
-void playGame(int time, std::mt19937 randomEngine) 
+void playGame() 
 {
-    int diceResult = rollDice(randomEngine); // サイコロを振る
     std::cout << "サイコロを振りました。奇数か偶数か当ててください（奇数: 1, 偶数: 2）: ";
 
     int userChoice;
     std::cin >> userChoice;
 
+    int diceResult = SetTimeout(rollDice, 3);
+
     // ランダムな出目とユーザーの選択をコールバック関数に渡し、結果を待つ
     std::function<bool(int, int)> isGuessCorrect = [](int result, int choice) {
         return (result % 2 == 1 && choice == 1) || (result % 2 == 0 && choice == 2);
         };
-
-    std::this_thread::sleep_for(std::chrono::seconds(time));
 
     if (isGuessCorrect(diceResult, userChoice)) {
         std::cout << "サイコロの出目 : " << diceResult << "\n" << "正解！\n" << std::endl;
@@ -55,12 +64,7 @@ bool checkGuess(int diceResult, int time, int userChoice)
 int main(void)
 {
 	SetConsoleOutputCP(65001);
-
-    // 乱数生成器の初期化
-    std::random_device seedGenerator;
-    std::mt19937 randomEngine(seedGenerator());
-
-    playGame(3,randomEngine);
+    playGame();
 
 	return 0;
 }
