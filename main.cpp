80a7945f58dc0d2d8af9d061d1d3122ac32346a8
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-12-25 00:04:41 +0900

    できたかもしれない

--- main.cpp
+++ main.cpp
@@ -2,11 +2,118 @@
 #include<iostream>
 #include<windows.h>
 
+#include<list>
+
 int main(void)
 {
 	SetConsoleOutputCP(65001);
 	char str[] = "あいうえお";
-	printf("%s", str);
+	
+	//char station[30][20] =
+	//{
+	//	"Tokyo",
+	//	"Kanda",
+	//	"Akihabara",
+	//	"Okachimati",
+	//	"Ueno",
+	//	"Uguisudani",
+	//	"Nippori",
+	//	/*"Nishi-Nippori",*/
+	//	"Tabata",
+	//	"Komagome",
+	//	"Sugamo",
+	//	"Otsuka",
+	//	"Ikebukuro",
+	//	"Mejiro",
+	//	"Takadanobaba",
+	//	"Shin-Okubo",
+	//	"Shinjuku",
+	//	"Yoyogi",
+	//	"Harajuku",
+	//	"Shibuya",
+	//	"Ebisu",
+	//	"Meguro",
+	//	"Gotanda",
+	//	"Osaki",
+	//	"Shinagawa",
+	///*	"Takanawa-Gateway",*/
+	//	"Tamachi",
+	//	"Hamamatsucho",
+	//	"Shimbashi",
+	//	"Yurakucho"
+	//};
+	
+
+	std::list<const char*>YamanoteLineStation =
+	{
+		"Tokyo",
+		"Kanda",
+		"Akihabara",
+		"Okachimati",
+		"Ueno",
+		"Uguisudani",
+		"Nippori",
+		/*"Nishi-Nippori",*/
+		"Tabata",
+		"Komagome",
+		"Sugamo",
+		"Otsuka",
+		"Ikebukuro",
+		"Mejiro",
+		"Takadanobaba",
+		"Shin-Okubo",
+		"Shinjuku",
+		"Yoyogi",
+		"Harajuku",
+		"Shibuya",
+		"Ebisu",
+		"Meguro",
+		"Gotanda",
+		"Osaki",
+		"Shinagawa",
+		/*	"Takanawa-Gateway",*/
+		"Tamachi",
+		"Hamamatsucho",
+		"Shimbashi",
+		"Yurakucho"
+	};
+
+	//1928年の駅を表示
+	std::cout << "YamanoteLineStation1928" << std::endl;
+	for (std::list<const char*>::iterator itr = YamanoteLineStation.begin(); itr != YamanoteLineStation.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
+
+	for (std::list<const char*>::iterator itr = YamanoteLineStation.begin(); itr != YamanoteLineStation.end(); ++itr)
+	{
+		if (*itr == "Tabata")
+		{
+			itr = YamanoteLineStation.insert(itr, "Nishi-Nippori");
+			++itr;
+		}
+	}
+
+	//1972年の駅を表示
+	std::cout << "\nYamanoteLineStation1972" << std::endl;
+	for (std::list<const char*>::iterator itr = YamanoteLineStation.begin(); itr != YamanoteLineStation.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
+
+	for (std::list<const char*>::iterator itr = YamanoteLineStation.begin(); itr != YamanoteLineStation.end(); ++itr)
+	{
+		if (*itr == "Tamachi")
+		{
+			itr = YamanoteLineStation.insert(itr, "Takanawa-Gateway");
+			++itr;
+		}
+	}
+
+
+	//2023年の駅を表示
+	std::cout << "\nYamanoteLineStation2023" << std::endl;
+	for (std::list<const char*>::iterator itr = YamanoteLineStation.begin(); itr != YamanoteLineStation.end(); ++itr) {
+		std::cout << *itr << std::endl;
+	}
 
 	return 0;
 }
